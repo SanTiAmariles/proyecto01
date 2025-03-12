8ce67445e64852cbda8f0374af96d9c934e8c726
SanTi Amariles <erik.amariles@correounivalle.edu.co>
2025-03-11 21:27:04 -0500

    Evitar ciclos dentro de la misma rama

--- no_informada/amplitud.py
+++ no_informada/amplitud.py
@@ -1,14 +1,17 @@
 from collections import deque
 
 class Nodo:
-    def __init__(self, matriz, x, y, cantPaquetes):
-        #Posición inicial del dron
+    def __init__(self, matriz, x, y, cantPaquetes, visitados=None):
+        # Posición inicial del dron
         self.posicionX = x
         self.posicionY = y
         self.matriz = matriz
-        #Paquetes por recoger
         self.faltan = cantPaquetes
         
+        # para registr las celdas visitadas en cada ruta
+        self.visitados_camino = visitados.copy() if visitados else set()
+        self.visitados_camino.add((x, y))  # Toma la celda como visitada
+
     def expandir(self):
         operadores = [
             (1, 0),  # Derecha
@@ -19,7 +22,7 @@ class Nodo:
 
         if self.faltan == 0:
             return ["Fin"]
-        elif (self.faltan >= 0):
+        elif self.faltan >= 0:
             vecinos = []
             for dx, dy in operadores:
                 nuevoX = self.posicionX + dx
@@ -28,14 +31,22 @@ class Nodo:
                 if 0 <= nuevoX < len(self.matriz) and 0 <= nuevoY < len(self.matriz[0]):
                     nuevaFaltan = self.faltan  
 
+                    if (nuevoX, nuevoY) in self.visitados_camino:
+                        continue 
+
+                    nuevaFaltan = self.faltan 
+
                     if self.matriz[nuevoX][nuevoY] == 4:  
                         nuevaFaltan -= 1
 
                     if self.matriz[nuevoX][nuevoY] == 4: 
-                        self.faltan -=1
+                        self.faltan -= 1
 
                     if self.matriz[nuevoX][nuevoY] != 1:  
-                        nuevaCelda = Nodo(self.matriz, nuevoX, nuevoY, self.faltan)
+                        nuevo_visitados_camino = self.visitados_camino.copy() # copia el historial de celdas visitadas del nodp actual.
+                        nuevo_visitados_camino.add((nuevoX, nuevoY)) # agrega la nueva posición para el nodo hijo.
+
+                        nuevaCelda = Nodo(self.matriz, nuevoX, nuevoY, self.faltan, nuevo_visitados_camino)
                         vecinos.append(nuevaCelda)
 
             return vecinos
@@ -49,15 +60,13 @@ def crearCola(matriz, x, y, cantPaquetes):
     while cola:
         nodoActual = cola.popleft()
         if nodoActual == "Fin":
-            print("Se recogieron todos los paquetes con exito")
+            print("Se recogieron todos los paquetes con éxito")
             return 1
         print(nodoActual.posicionX, ",", nodoActual.posicionY)
         vecinos = nodoActual.expandir()
         cola.extend(vecinos)
-        
-    
-    
 
+    
 matriz = [
     [1, 1, 0, 0, 0, 0, 0, 1, 1, 1],
     [1, 1, 0, 1, 0, 1, 0, 1, 1, 1],
